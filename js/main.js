638393a7e56b45de8c8981a5f54fb5e771ed2cfc
Roman Jelínek <jelinek82@gmail.com>
2022-05-27 16:27:41 +0200

    First commit

---
+++ js/main.js
@@ -0,0 +1,51 @@
+jQuery(document).ready(function ($) {
+  $.getJSON("./json/points.json", function (data) {
+    let arr = $.map(data, function (item) {
+      let boxClass = `pointsBox${item.id}`;
+      $(".container").append(
+        `<div class="productWrapper"><img src="${item.url}" alt="Preview image" /><ul class=${boxClass}></ul></div>`
+      );
+      let points = $.map(item.points, function (point) {
+        $(`.${boxClass}`).append(
+          `<li class="singlePoint" style="top:${point.top}%; left:${point.left}%">
+            <a class="imgReplace" href="#0">Více</a>
+            <div class="moreInfo ${point.oriented}">
+              <h2>${point.product.name}</h2>
+              <div class="pointImage">
+                <img src="${point.product.image}" alt="Preview image" />
+              </div>
+              <p>${point.product.price} ${point.product.currency}</p>
+              <a href=${point.product.url} class="detailButton">Detail</a>
+              <a href="#0" class="closeInfo imgReplace">Zavřít</a>
+            </div>
+          </li>`
+        );
+      });
+    });
+    $(".singlePoint")
+      .children("a")
+      .on("click", function (e) {
+        e.preventDefault();
+        var selectedPoint = $(this).parent("li");
+        if (selectedPoint.hasClass("isOpen")) {
+          selectedPoint.removeClass("isOpen").addClass("visited");
+        } else {
+          selectedPoint
+            .addClass("isOpen")
+            .siblings(".single-point.isOpen")
+            .removeClass("isOpen")
+            .addClass("visited");
+        }
+      });
+    $(".closeInfo").on("click", function (event) {
+      event.preventDefault();
+      $(this)
+        .parents(".singlePoint")
+        .eq(0)
+        .removeClass("isOpen")
+        .addClass("visited");
+    });
+  }).fail(function () {
+    console.log("An error has occurred.");
+  });
+});
