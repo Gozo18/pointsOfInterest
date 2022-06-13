ab6bf7aebf2b37ab9810ddc5f6f5ea1961099143
Roman Jelínek <jelinek82@gmail.com>
2022-06-13 11:10:31 +0200

    Open close changes + prefix added

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 jQuery(document).ready(function ($) {
+  /* Načtení JSONu a append POI do obrázků */
   $.getJSON("./json/points.json", function (data) {
     let arr = $.map(data, function (item) {
-      let boxClass = `pointsBox${item.id}`;
+      let boxClass = `poi-pointsBox${item.id}`;
       $(".container").append(
-        `<div class="productWrapper"><img src="${item.url}" alt="Preview image" /><ul class=${boxClass}></ul></div>`
+        `<div class="poi-imageWrapper"><img src="${item.url}" alt="Preview image" /><ul class=${boxClass}></ul></div>`
       );
       let points = $.map(item.points, function (point) {
         var stock;
@@ -13,48 +14,55 @@ jQuery(document).ready(function ($) {
           stock = "Skladem";
         }
         $(`.${boxClass}`).append(
-          `<li class="singlePoint" style="top:${point.top}%; left:${point.left}%">
-            <a class="imgReplace" href="#0">Více</a>
-            <div class="moreInfo ${point.oriented}">
+          `<li class="poi-singlePoint" style="top:${point.top}%; left:${point.left}%">
+            <a class="poi-imgReplace" href="#0">Více</a>
+            <div class="poi-moreInfo ${point.oriented}">
               <h2>${point.product.name}</h2>
-              <div class="pointImage">
+              <div class="poi-pointImage">
                 <img src="${point.product.image}" alt="Preview image" />
               </div>
-              <div class="pointText">
+              <div class="poi-pointText">
                 <p>${point.product.price} ${point.product.currency}</p>
                 <p>${stock}</p>
               </div>
-              <a href=${point.product.url} class="detailButton">Detail</a>
-              <a href="#0" class="closeInfo imgReplace">Zavřít</a>
+              <a href=${point.product.url} class="poi-detailButton">Detail</a>
+              <a href="#0" class="poi-closeInfo poi-imgReplace">Zavřít</a>
             </div>
           </li>`
         );
       });
     });
-    $(".singlePoint")
+
+    /* Klik na POI element */
+    $(".poi-singlePoint")
       .children("a")
       .on("click", function (e) {
         e.preventDefault();
         var selectedPoint = $(this).parent("li");
+        $(".poi-singlePoint.isOpen").addClass("visited");
         if (selectedPoint.hasClass("isOpen")) {
-          selectedPoint.removeClass("isOpen").addClass("visited");
+          $(".poi-singlePoint").removeClass("isOpen");
         } else {
-          $(".singlePoint").removeClass("isOpen");
-          selectedPoint
-            .addClass("isOpen")
-            .siblings(".single-point.isOpen")
-            .removeClass("isOpen")
-            .addClass("visited");
+          $(".poi-singlePoint").removeClass("isOpen");
+          selectedPoint.addClass("isOpen");
         }
       });
-    $(".closeInfo").on("click", function (event) {
+
+    /* Mobilní zavírání */
+    $(".poi-closeInfo").on("click", function (event) {
       event.preventDefault();
       $(this)
-        .parents(".singlePoint")
+        .parents(".poi-singlePoint")
         .eq(0)
         .removeClass("isOpen")
         .addClass("visited");
     });
+
+    /* Pro první POI ze všech obrázků */
+    $(".poi-singlePoint:first").addClass("isOpen");
+
+    /* Pro první POI každého obrázku */
+    /* $(".poi-singlePoint:first-child").addClass("isOpen"); */
   }).fail(function () {
     console.log("An error has occurred.");
   });
